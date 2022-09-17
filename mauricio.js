2b40adace6d257a3f6e35d7e577bdceef3948a8c
Ian <ianhnizdo@gmail.com>
2022-09-17 12:15:59 -0400

    server updates

---
+++ mauricio.js
@@ -0,0 +1,1 @@
+//This is a test coming from Mauricio's computer
--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 //This is a test
 //Type in your last name and upload
-//Emin
 //Ian Robert Hnizdo
+<<<<<<< HEAD
 //Mauricio
 //Linden
 //Santiago
 //Another line of code
+=======
+//Emin Tahirov 
+//Ian
+//Santiago Gil Maya
+//Mauricio Castro
+//Linden Young
+>>>>>>> ac650677b8b88fc0bdbeb10eaf80fc120d579cf9
