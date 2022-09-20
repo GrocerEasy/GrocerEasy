aff30699c00a19c4b50aa5bb195358f164b4bf65
cmloof <cmlooff@gmail.com>
2022-09-20 16:08:11 -0400

    fixing routes and server.js files

--- server/routes/krogerRouter.js
+++ server/routes/krogerRouter.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const groceryData = require('../controllers/groceryController');
-
+const krogerController = require('../controllers/krogerController');
 const router = express.Router();
 
-//Go to sql database. Collect items if there else go to api connection to collect data and save it.
-router.get('/', groceryData.getFood,)
+// Route is /api
 
+//Go to sql database. Collect items if there else go to api connection to collect data and save it.
+router.get('/', krogerController.getToken, (req, res) => {
+  return res.status(200).json({
+    success: true
+  });
+});
+
+module.exports = router;
