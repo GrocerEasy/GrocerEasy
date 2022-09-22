f9386a4851d200592d2c3ddee7b9e64135f7602b
JP Moregain <j.p.moregain15@gmail.com>
2022-09-22 18:07:16 -0400

    Style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, Link } from 'react-router-dom';
 import './componentStylesheets/Header.css'
 import Logo from '../img/logo-color.png'
-
+// Redux
+import { useSelector } from 'react-redux';
+// access whether a user is logged in by logging in, or whether they have authenticated right after registering an account
 
 const Header = (props) => {
+  const userLogin = useSelector((state) => state.userLogin)
+  const userRegister = useSelector((state) => state.userRegister)
+  const [auth, setAuth] = useState(false);
+  const [username, setUsername] = useState('')
+
+  // deconstruct store state for userLogin
+  const { loading, error, userInfo } = userLogin;
+  if (userInfo) {
+    setUsername(userInfo.user.username);
+    setAuth(true);
+  }
+  // if (userInfo.user.username) {
+  //   setUsername(userInfo.user.username);
+  // }
+  // if (userInfo.accessToken) {
+  //   setAuth(true);
+  // }
+  // const { loading, error, userInfo } = userRegister;
+  // useEffect(() => {
+  //   setAuth(true);
+  // }, [userInfo.accessToken])
+
+  // useEffect(() => {
+  //   setUsername(userInfo.user.username);
+  // }, [userInfo.user.username]);
+
+
   return (
     <header className="header">
       <div className='location'>Your Location: </div>
