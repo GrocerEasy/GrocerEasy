0b11eae6bd6302d9c448b44fcd1e5cf0956bf8d7
JP Moregain <j.p.moregain15@gmail.com>
2022-09-22 18:35:30 -0400

    Add conditional rendering for authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,46 +9,48 @@ import { useSelector } from 'react-redux';
 const Header = (props) => {
   const userLogin = useSelector((state) => state.userLogin)
   const userRegister = useSelector((state) => state.userRegister)
-  const [auth, setAuth] = useState(false);
-  const [username, setUsername] = useState('')
 
   // deconstruct store state for userLogin
   const { loading, error, userInfo } = userLogin;
-  if (userInfo) {
-    setUsername(userInfo.user.username);
-    setAuth(true);
+
+  const logout = () => {
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('uid');
   }
-  // if (userInfo.user.username) {
-  //   setUsername(userInfo.user.username);
-  // }
-  // if (userInfo.accessToken) {
-  //   setAuth(true);
-  // }
-  // const { loading, error, userInfo } = userRegister;
-  // useEffect(() => {
-  //   setAuth(true);
-  // }, [userInfo.accessToken])
 
-  // useEffect(() => {
-  //   setUsername(userInfo.user.username);
-  // }, [userInfo.user.username]);
-
-
-  return (
-    <header className="header">
+  if (userInfo) {
+    return (
+      <header className="header">
       <div className='location'>Your Location: </div>
       <div className='logoContainer'>
         <img src={Logo}/>
       </div>
-      <nav className='links'>
-        <Link className="header-login-signup-button" to='/login'>Log In</Link>
-        <Link className="header-login-signup-button" to='/signup'>Sign Up</Link>
+      <nav className='authLinks'>
+        <button className='header-login-signup-button' onClick={logout}>Log Out</button>
         <Link className="header-login-signup-button" to='/'>Home</Link>
+        <div className="header-login-signup-button">Welcome {userInfo.user.username}</div>
       </nav>
       <Outlet />
     </header>
-  )
-};
+    )
+  }
+  else {
+    return (
+      <header className="header">
+        <div className='location'>Your Location: </div>
+        <div className='logoContainer'>
+          <img src={Logo}/>
+        </div>
+        <nav className='links'>
+          <Link className="header-login-signup-button" to='/login'>Log In</Link>
+          <Link className="header-login-signup-button" to='/signup'>Sign Up</Link>
+          <Link className="header-login-signup-button" to='/'>Home</Link>
+        </nav>
+        <Outlet />
+      </header>
+    )
+  };
+}
 
 // Default userName to there! in case a username is not passed
 Header.defaultProps = {
